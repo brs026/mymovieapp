4c41e3938f49baefa6b09fac016771f332ed48cd
brs026 <brs026@motorolasolutions.com>
2022-04-19 16:31:07 +0300

    IMovieRepository initial version

--- repository/imovierepository.go
+++ repository/imovierepository.go
@@ -1,7 +1,12 @@
 package repository
 
-type IMovieRepository interface {
-	//GetMovies() ([]model.Movie, error)
-	//GetMovie(id int) (model.Movie, error )
+import "github.com/brs026/mymovieapp/model"
 
+type IMovieRepository interface {
+	GetMovies() ([]model.Movie, error)
+	GetMovie(id int) (model.Movie, error)
+	DeleteMovie(id int) error
+	DeleteMovies() error
+	CreateMovie(model.Movie) error
+	UpdateMovie(id int, movie model.Movie) error
 }
